0e3e2fedcd61a9547712ca664ba90128ca2bd9fc
Gauthier Delamarre <gdelamarre@gmail.com>
2023-02-07 11:05:06 +0100

    Update Bundle to match new interface

--- src/DoctrineMultilingualBundle.php
+++ src/DoctrineMultilingualBundle.php
@@ -6,20 +6,15 @@ namespace Gauthier\MultilingualBundle;
 
 use Gauthier\MultilingualBundle\DependencyInjection\CompilerPass\DoctrineMultilingualCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class DoctrineMultilingualBundle extends Bundle
 {
 
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new DependencyInjection\DoctrineMultilingualExtension();
-    }
-
-    public function build(ContainerBuilder $container)
-    {
-        $container->addCompilerPass(new DoctrineMultilingualCompilerPass());
-        parent::build($container);
     }
 
 }
