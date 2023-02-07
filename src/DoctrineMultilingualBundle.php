50383552a2a1a7f8824642094ba248c8c459e07e
Gauthier Delamarre <gdelamarre@gmail.com>
2023-02-07 11:39:01 +0100

    Register Type in Bundle

--- src/DoctrineMultilingualBundle.php
+++ src/DoctrineMultilingualBundle.php
@@ -1,16 +1,18 @@
 <?php
-
-
 namespace Gauthier\MultilingualBundle;
 
-
-use Gauthier\MultilingualBundle\DependencyInjection\CompilerPass\DoctrineMultilingualCompilerPass;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Doctrine\DBAL\Types\Type;
+use Gauthier\MultilingualString\Type\MultilingualStringType;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class DoctrineMultilingualBundle extends Bundle
 {
+    public function __construct()
+    {
+        Type::addType('multilingual_string', MultilingualStringType::class);
+    }
+
 
     public function getContainerExtension(): ?ExtensionInterface
     {
